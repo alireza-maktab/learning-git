9b5bd7dce4836633553af907e5bfc7a5abce89c8
Alireza <alirezamolaii@gmail.com>
2021-12-02 13:12:49 +0330

    add new file

---
+++ switch-break.js
@@ -0,0 +1,27 @@
+const personStates = {
+    SLEEP: Symbol.for('sleep'),
+    NORMAL: Symbol.for('Abbas')
+}
+
+console.log(Symbol.keyFor(personStates.NORMAL) === 'Abbas')
+
+
+// const person = {
+//     state: personStates.NORMAL,
+
+//     say(s) {
+//         switch (this.state) {
+//             case personStates.SLEEP:
+//                 console.log('zzzzzz')
+//                 break;
+//             case personStates.NORMAL:
+//                 console.log(s);
+//                 break;
+//             default:
+//                 throw 'bad state'
+//         }
+//     }
+// }
+// person.say("Hello")
+
+
